a9df7599bdf9af1fb27970dfd55dea9e46087cd7
Haythemus <i-tem@live.fr>
2018-11-28 17:51:04 +0100

    Chiffre Mystere Challenger Mode fini a 100% et fonctionnel Chiffre Mystere Defenseur Mode fini a 100% et fonctionnel Chiffre Mystere Duel Mode fini a 100% et fonctionnel Master Mind Challenger mode fini a 100% et fonctionnel

--- src/com/projet3/mastermind/MasterMindChallengerMode.java
+++ src/com/projet3/mastermind/MasterMindChallengerMode.java
@@ -1,5 +1,6 @@
 package com.projet3.mastermind;
 
+import com.projet3.opc.Main;
 import com.projet3.traitementcalcul.TraitementEtCalcul;
 import com.projet3.menu.MenuGameSelection;
 
@@ -11,16 +12,60 @@ import java.util.InputMismatchException;
 public class MasterMindChallengerMode extends TraitementEtCalcul{
 
     public void runMasterMindChallengerMode(){
+       //Lecture du fichier de configuration
+        runConfiguration();
+
+        //Affichage du mode de jeux
         System.out.println("**********        *******************************                     *******************");
         System.out.println("**********        **Master Mind Challenger Mode**                     *******************");
         System.out.println("**********        *******************************                     *******************");
 
+        //Pré configuration de la partie
+        if (configurationJeux.equalsIgnoreCase("false")){
+            chiffreMystereOrdinateur = generateNumber();
+        }else{
+            chiffreMystereOrdinateur = generateNumber(nbrCases);
 
+        }
+
+        //Permet d'afficher la solution en mode développeur
+        if ( modeDeveloppeur.equalsIgnoreCase("On") || Main.modeDeveloppeur.equalsIgnoreCase("Deoppeur")){
+            System.out.println("Le combinaison généré par l'ordinateur est  " + chiffreMystereOrdinateur);
+        }
+        tabChiffreMystereOrdinateur = decoupeChiffreMystereOrdinateur(chiffreMystereOrdinateur);
 
 
         do {
+            compteur++;
+
+            //Partie joueur
+            if (configurationJeux.equalsIgnoreCase("false")) {
+                do {
+                    System.out.println("Entrez une combinaison à 4 chiffres");
+                    propositionChiffreJoueur = sc.nextLine();
+                    nbrEssai = 10;
+                } while (!propositionChiffreJoueur.matches(regex) || propositionChiffreJoueur.length() != 4);
+            } else {
+                do {
+                    System.out.println("Entrez une combinaison à " + nbrCases + " chiffres");
+                    propositionChiffreJoueur = sc.nextLine();
+                } while (!propositionChiffreJoueur.matches(regex) || propositionChiffreJoueur.length() != nbrCases);
+            }
+            tabPropositionChiffreJoueur = decoupePropositionChiffreJoueur(propositionChiffreJoueur);
 
 
+            //Comparaison et calcul
+            compareMasterMind(tabChiffreMystereOrdinateur, tabPropositionChiffreJoueur, propositionChiffreJoueur);
+        }while (!propositionChiffreJoueur.equalsIgnoreCase(chiffreMystereOrdinateur) && compteur != nbrEssai);
+
+        if (propositionChiffreJoueur.equalsIgnoreCase(chiffreMystereOrdinateur)){
+            System.out.println("Bravo vous avez gagné!!!!!");
+        }else{
+            System.out.println("Dommage vous avez perdu!! la réponse était " + chiffreMystereOrdinateur);
+        }
+
+        //Affichage Menu fin de Jeux
+        do {
             menuGameSelection.displayEndGameSelection();
             System.out.println("Veuillez faire un choix svp");
 
@@ -31,10 +76,7 @@ public class MasterMindChallengerMode extends TraitementEtCalcul{
             }
             sc.nextLine();
         }while (choixFinJeux < 1 || choixFinJeux > 3);
-
         menuGameSelection.selectedEndGameMode(2,1,choixFinJeux);
-
-
     }
 
 
